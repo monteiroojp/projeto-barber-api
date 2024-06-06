9dab843c1a3ead889089229f0120156df45e51c3
João Pedro Monteiro <154384458+monteiroojp@users.noreply.github.com>
2024-06-06 16:52:59 -0300

    .

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
-//Express errors
+// Express errors
 require('express-async-errors')
 
-//Env data
+// Env data
 require('dotenv').config()
 
-//App
+// App
 const express = require('express')
 const app = express()
 
-//Import routes
-const loginRoute = require('./routes/loginRoute.js')
-const appoimentRoute = require('./routes/appointmentRoute.js')
-const barberRoute = require('./routes/barberRoute.js')
-const serviceRoute = require('./routes/servicesRoute.js')
+// Extra security imports
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 
-//Extract data from requests
+// Middlewares import
+const errorHandler = require('./middlewares/errorHandler.js')
+const notFound = require('./middlewares/notFound.js')
+
+// Security
+app.use(helmet())
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    optionsSuccessStatus: 200
+}))
+app.use(xss())
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}))
+
+// Extract data from requests
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-//Routes middleware
-const authToken = require('./middlewares/authToken.js')
-
-//Publics
+// Public routes
 app.use('/login', express.static('./public/login/'))
 app.use('/', express.static('./public/home/'))
 app.use('/signUp', express.static('./public/signUp'))
@@ -29,40 +42,27 @@ app.use('/forgotPassword', express.static('./public/forgotPassword'))
 app.use('/myAppoiments', express.static('./public/myAppoiments'))
 app.use('/dashboard', express.static('./public/dashboard'))
 
-//Routes
+// Routes middleware
+const authToken = require('./middlewares/authToken.js')
+
+// Import routes
+const loginRoute = require('./routes/loginRoute.js')
+const appoimentRoute = require('./routes/appointmentRoute.js')
+const barberRoute = require('./routes/barberRoute.js')
+const serviceRoute = require('./routes/servicesRoute.js')
+
+// Routes
 app.use('/api/v1/auth', loginRoute)
-app.use('/api/v1/appoiments', authToken,appoimentRoute)
+app.use('/api/v1/appoiments', authToken, appoimentRoute)
 app.use('/api/v1/barbers', authToken, barberRoute)
 app.use('/api/v1/services', serviceRoute)
 
-//Extra securites import
-const helmet = require('helmet')
-const cors = require('cors')
-const xss = require('xss-clean')
-const rateLimiter = require('express-rate-limit')
-
-//Middlewares import
-const errorHandler = require('./middlewares/errorHandler.js')
-const notFound = require('./middlewares/notFound.js')
-
-//Middlewares
+// Error handling middlewares
 app.use(errorHandler)
 app.use(notFound)
 
-//Security 
-app.use(helmet())
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    optionsSuccessStatus: 200
-  }))
-app.use(xss())
-app.use(rateLimiter({
-  windowMs: 15 * 60 * 1000,
-  max: 100
-}))
-
-//Start setup
-const port = process.env.PORT || 3000;
+// Start setup
+const port = process.env.PORT || 3000
 const connectDb = require('./db/connectDB')
 
 const start = async () => {
